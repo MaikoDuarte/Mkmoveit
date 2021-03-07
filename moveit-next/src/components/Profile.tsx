1198b8ef2b4019db2aa841fc3b30afdf2e128208
mkduhub <mkdu185@gmail.com>
2021-03-07 19:45:44 -0300

    segundo commit

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/components/Profile.module.css'
+export function Profile () {
+    return (
+    <div className={styles.profileContainer}>
+        <div>
+            <img src="https://i.pinimg.com/236x/00/3a/06/003a06fd0f7f44e0184dddc0759974cf.jpg" alt="stark"/>
+            <strong>Tony Stark</strong>
+            <p>
+                <img src="icons/level.svg" alt="Level"/>
+                Level 1
+                
+           </p>
+
+      
+    </div>
+    
+
+    </div>
+    );
+
+}
